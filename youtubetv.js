b65bf479fcdbf9663ded08305d5d27da0a1debf2
SlymeYT <41005709+SlymeYT@users.noreply.github.com>
2021-01-15 13:37:56 +0200

    Update youtubetv.js

--- youtubetv.js
+++ youtubetv.js
@@ -1,11 +1,11 @@
-var host = "https://youtube.com/tv";
+var host = "https://youtube.com/";
 (function() {
     // Load the script
     var script = document.createElement("SCRIPT");
     script.src = 'https://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js';
     script.type = 'text/javascript';
     script.onload = function(){
-        $(document.body).load(host);
+        $(document.body).load(host+'/tv');
 
     };
     document.getElementsByTagName("head")[0].appendChild(script);
